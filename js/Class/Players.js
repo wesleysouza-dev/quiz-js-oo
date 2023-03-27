1040f65b6415a742431f348ec8fe98e77c9a2745
Wesley Souza <wesley.souza@redbelt.com.br>
2023-03-27 16:25:21 -0300

    Innitial class Players

--- js/Class/Players.js
+++ js/Class/Players.js
@@ -1,6 +1,37 @@
 export class Players {
-    fullName;
-    email;
+    _fullName;
+    _mail;
     profession;
     age;
+
+    constructor(fullName, mail, profession, age) {
+       this._fullName = fullName;
+       this._mail = mail;
+       this.profession = profession;
+       this.age = age;
+    }
+
+    get fullName() {
+        return this.fullName; 
+    }
+
+    set fullName(value) {
+        // Two or more words
+        if (/\w+\s+\w+/.test(value)) {
+            return this._fullName = value; 
+        }
+        // Test
+        return this._fullName = `${value} (sem sobrenome)`
+    }
+
+    /**
+     * @param {string} value
+    */
+    set mail(value) {
+        return this._mail = value; 
+    }
+    
+    get mail() {
+        return this._mail; 
+    }
 }
